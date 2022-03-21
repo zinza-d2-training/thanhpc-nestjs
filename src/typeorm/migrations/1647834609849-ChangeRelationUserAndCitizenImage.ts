fb6e8c5fa3349336e642bc552cc9ecfd8ecf64f3
Thanh pham <thanhpham.23102000@gmail.com>
2022-03-21 10:54:23 +0700

    change relation beetwen user table and citizenImage table

---
+++ src/typeorm/migrations/1647834609849-ChangeRelationUserAndCitizenImage.ts
@@ -0,0 +1,48 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class ChangeRelationUserAndCitizenImage1647834609849
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'citizen_images',
+      new TableColumn({
+        name: 'user_id',
+        type: 'int',
+        length: '11',
+      }),
+    );
+    await queryRunner.createForeignKey(
+      'citizen_images',
+      new TableForeignKey({
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      }),
+    );
+    // delete column citizen_image_id
+    const table = await queryRunner.getTable('users');
+    const foreignKey = table.foreignKeys.find(
+      (fk) => fk.columnNames.indexOf('citizen_image_id') !== -1,
+    );
+    await queryRunner.dropForeignKey('users', foreignKey);
+    await queryRunner.dropColumn('users', 'citizen_image_id');
+    await queryRunner.dropColumn('citizen_images', 'name');
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable('citizen_images');
+    const foreignKey = table.foreignKeys.find(
+      (fk) => fk.columnNames.indexOf('user_id') !== -1,
+    );
+    await queryRunner.dropForeignKey('citizen_images', foreignKey);
+    await queryRunner.dropColumn('citizen_images', 'user_id');
+  }
+}
