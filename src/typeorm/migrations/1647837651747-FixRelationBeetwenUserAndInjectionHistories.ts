c9ccfc6527c8e496ea71241263930f2877d6a073
Thanh pham <thanhpham.23102000@gmail.com>
2022-03-21 11:48:41 +0700

    Fix relation beetwen user table and injection_histories table

---
+++ src/typeorm/migrations/1647837651747-FixRelationBeetwenUserAndInjectionHistories.ts
@@ -0,0 +1,47 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class FixRelationBeetwenUserAndInjectionHistories1647837651747
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'injection_histories',
+      new TableColumn({
+        name: 'user_id',
+        type: 'int',
+        length: '11',
+      }),
+    );
+    await queryRunner.createForeignKey(
+      'injection_histories',
+      new TableForeignKey({
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      }),
+    );
+    // delete column injection_history_id
+    const table = await queryRunner.getTable('users');
+    const foreignKey = table.foreignKeys.find(
+      (fk) => fk.columnNames.indexOf('injection_history_id') !== -1,
+    );
+    await queryRunner.dropForeignKey('users', foreignKey);
+    await queryRunner.dropColumn('users', 'injection_history_id');
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable('injection_histories');
+    const foreignKey = table.foreignKeys.find(
+      (fk) => fk.columnNames.indexOf('user_id') !== -1,
+    );
+    await queryRunner.dropForeignKey('injection_histories', foreignKey);
+    await queryRunner.dropColumn('injection_histories', 'user_id');
+  }
+}
