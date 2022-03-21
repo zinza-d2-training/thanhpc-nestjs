a02f336342c72e10a321233eefc37a2445aeb06b
Thanh pham <thanhpham.23102000@gmail.com>
2022-03-21 14:20:46 +0700

    edit querry

--- src/typeorm/migrations/1647836761201-ChangeTypeColumnsInUser.ts
+++ src/typeorm/migrations/1647836761201-ChangeTypeColumnsInUser.ts
@@ -4,17 +4,19 @@ export class ChangeTypeColumnsInUser1647836761201
   implements MigrationInterface
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('ALTER TABLE "users" ALTER Column "dob" date');
+    await queryRunner.query('ALTER TABLE "users" MODIFY COLUMN "dob" date');
     await queryRunner.query(
-      'ALTER TABLE "users" ALTER Column "gender" tinyint(4)',
+      'ALTER TABLE "users" MODIFY COLUMN "gender" tinyint(4)',
     );
     await queryRunner.dropColumn('users', 'status_injection_registration');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('ALTER TABLE "users" ALTER Column "dob" timestamp');
     await queryRunner.query(
-      'ALTER TABLE "users" ALTER Column "gender" varchar(11)',
+      'ALTER TABLE "users" MODIFY COLUMN "dob" timestamp',
+    );
+    await queryRunner.query(
+      'ALTER TABLE "users" MODIFY COLUMN "gender" varchar(11)',
     );
     await queryRunner.addColumn(
       'users',
